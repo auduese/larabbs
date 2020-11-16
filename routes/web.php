08d55eaba13c82f7f18fb41ec0e6bcf72b086977
auduese <18300230330@163.com>
2020-11-16 03:58:30 +0000

    邮箱认证

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'PagesController@root')->name('root');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes(['verify' => true]);
